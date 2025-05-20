383e59a40343fe63fdadf52e3271a34fe7e3e158
Abdelazizkb <61007391+Abdelazizkb@users.noreply.github.com>
2025-05-20 16:06:54 +0100

    #1 feat(dashboard) : add routing system

---
+++ src/app/router.tsx
@@ -0,0 +1,17 @@
+import { lazy } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import NoMatch from "../layout/errors/no-match";
+
+const Auth = lazy(() => import("../features/auth"));
+
+const AppRouter = () => {
+  return (
+    <Routes>
+      <Route index element={<Navigate to="auth" />} />
+      <Route path="/auth/*" element={<Auth />} />
+      <Route path="*" element={<NoMatch />} />
+    </Routes>
+  );
+};
+
+export default AppRouter;
