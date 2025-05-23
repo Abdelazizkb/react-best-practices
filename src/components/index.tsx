69adcef4e9c3d808daec788d0c9aca1ce49ce58a
Abdelazizkb <61007391+Abdelazizkb@users.noreply.github.com>
2025-05-23 01:30:47 +0100

    almost done

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,3 @@
 export { default as Loader } from "./loaders";
 export { default as Logo } from "./logo";
+export { default as Input } from "./input";
--- src/features/auth/login/index.tsx
+++ src/features/auth/login/index.tsx
@@ -1,5 +1,19 @@
+import { Input } from "components";
+import styles from "./styles.module.scss";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className={styles.loginPage}>
+      <div className={styles.header}>
+        <p className="heading-3">Welcome back.</p>
+        <p className="heading-3">Log in to your account below.</p>
+      </div>
+      <form action="">
+        <Input placeholder="Name@example.com" />
+        <Input placeholder="Password" />
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
